3f73e0e6d59e2d582912e9ea5447b50c3e41a74e
Tim Heckman <tim+code@timheckman.net>
2014-02-12 10:39:13 -0800

    the day we fight back is over, remove banner

--- static/js/thedaywefightback.js
+++
@@ -1,3 +0,0 @@
-<!--[if !(lt IE 8)]><!-->
-    (function(){var e=document.createElement("script");e.type="text/javascript";e.async=true;e.src=document.location.protocol+"//d1agz031tafz8n.cloudfront.net/thedaywefightback.js/widget.min.js";var t=document.getElementsByTagName("script")[0];t.parentNode.insertBefore(e,t)})()
-<!--<![endif]-->
